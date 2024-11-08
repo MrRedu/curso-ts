a9129a9918535c0b715d296c941d063b4e146e21
Eduardo <ejrb1234@gmail.com>
2024-11-07 20:00:53 -0400

    Inferior tipos y modo estricto

--- 01-bases/app.js
+++ 01-bases/app.js
@@ -1,17 +1,30 @@
 "use strict";
-const message = "hola mundo"; // const message: "hola mundo"
-let msg = "hola mundo"; // let msg: string
-// msg = 123; // Type 'number' is not assignable to type 'string'.
-const mensaje = "Hello world";
-console.log(mensaje);
-// const PersonType: {
-//   name: string;
-//   age: number;
-// }
-const person = {
-    name: "John Doe",
-    age: 25,
-};
-// person.age = '26'; // Type 'string' is not assignable to type 'number'.
-person.age = 26;
-console.log(person.age);
+(() => {
+    const a = 10; // const a: 10 => El tipo de dato es 10; porque es una constante (const)
+    let b = 10; // let b: number => El tipo de dato es number; porque es una variable (let)
+    //* Siempre se debe especificar el tipo de dato
+    let k = 10;
+    let l;
+    l = 3.14159265;
+    //! l = 'prueba' // Type 'string' is not assignable to type 'number'
+    //* Difiere el tipo :any
+    let i; //: any
+    i = "hola";
+    i = 16;
+    i = true;
+    i = null;
+    /*
+    function sayHello1(msg) {
+      console.log(msg + 2);
+      msg.???
+    }
+    ! Parameter 'msg' implicitly has an 'any' type. => Esto es por la regla
+    * {"noImplicitAny": true} del archivo tsconfig.json
+    sayHello1("hola");
+    */
+    function sayHello2({ msg }) {
+        console.log(msg);
+        // msg.TodosLosMétodosDelTipoString;
+    }
+    sayHello2({ msg: "hola" });
+})();
--- 01-bases/app.ts
+++ 01-bases/app.ts
@@ -1,22 +1,34 @@
-const message = "hola mundo"; // const message: "hola mundo"
-let msg = "hola mundo"; // let msg: string
-// msg = 123; // Type 'number' is not assignable to type 'string'.
+(() => {
+  const a = 10; // const a: 10 => El tipo de dato es 10; porque es una constante (const)
+  let b = 10; // let b: number => El tipo de dato es number; porque es una variable (let)
 
-const mensaje: string = "Hello world";
-console.log(mensaje);
+  //* Siempre se debe especificar el tipo de dato
+  let k: number = 10;
 
-// const PersonType: {
-//   name: string;
-//   age: number;
-// }
+  let l: number;
+  l = 3.14159265;
+  //! l = 'prueba' // Type 'string' is not assignable to type 'number'
 
-const person = {
-  name: "John Doe",
-  age: 25,
-};
+  //* Difiere el tipo :any
+  let i; //: any
+  i = "hola";
+  i = 16;
+  i = true;
+  i = null;
 
-// person.age = '26'; // Type 'string' is not assignable to type 'number'.
+  /*
+  function sayHello1(msg) {
+    console.log(msg + 2);
+    msg.???
+  }
+  ! Parameter 'msg' implicitly has an 'any' type. => Esto es por la regla
+  * {"noImplicitAny": true} del archivo tsconfig.json
+  sayHello1("hola");
+  */
 
-person.age = 26;
-
-console.log(person.age);
+  function sayHello2({ msg }: { msg: string }) {
+    console.log(msg);
+    // msg.TodosLosMétodosDelTipoString;
+  }
+  sayHello2({ msg: "hola" });
+})();
